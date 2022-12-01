e0337b44ba4ac788bf14bfae0d9bdd24acdf1837
Peter Mikkelsen <peter.mikkelsen@paymenttools.com>
2022-12-01 23:08:24 +0100

    solved day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+import java.io.File
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun main() {
+    fun part1(input: List<List<Int>>) = input.maxOf { it.sum() }
+
+    fun part2(input: List<List<Int>>) = input.map(List<Int>::sum).sortedDescending().take(3).sum()
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readDayOneInput("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readDayOneInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+private fun readDayOneInput(name: String): List<List<Int>> {
+    return File("src", "$name.txt").readText()
+        .split("\n\n")
+        .map { it.replace("\n", " ") }
+        .format()
+}
+private fun List<String>.format(): List<List<Int>> {
+    return this.map { it.split(" ").map(String::toInt) }
 }
